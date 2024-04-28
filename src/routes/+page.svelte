37375bcb817cf5162dc1a3619fbc30cfbf3318b7
Razvan <tangent.jotey@gmail.com>
2024-04-29 00:15:27 +0200

    chore(app): using plain css for example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,46 @@
+<style>
+  .wrapper {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    background: black;
+  }
+</style>
+
 <script>
   import Animated from '$lib/components/Animated.svelte'
   import PixelMap from '$lib/components/PixelMap.svelte'
   const FRAMES = [
     `
-  . x . . . . . x .
-  . . x . . . x . .
-  . . x x x x x . .
-  . x x x x x x x .
-  . x x x x x x x .
-  . . x x x x x . .
-  . x . . . . . x .
-  . . x . . . x . .
-  `,
+      . x . . . . . x .
+      . . x . . . x . .
+      . . x x x x x . .
+      . x x x x x x x .
+      . x x x x x x x .
+      . . x x x x x . .
+      . x . . . . . x .
+      . . x . . . x . .
+      `,
     `
-  . . . x . x . . .
-  . . x . . . x . .
-  . . x x x x x . .
-  . x x x x x x x .
-  . x x x x x x x .
-  . . x x x x x . .
-  . . x x . x x . .
-  . . . . . . . . .
-  `,
+      . . . x . x . . .
+      . . x . . . x . .
+      . . x x x x x . .
+      . x x x x x x x .
+      . x x x x x x x .
+      . . x x x x x . .
+      . . x x . x x . .
+      . . . . . . . . .
+      `,
   ]
 </script>
 
-<div
-  class="fixed left-0 right-0 top-0 bottom-0 grid justify-items-center items-center"
->
-  <Animated frames={FRAMES} interval={1000} let:using={{ map }}>
+<div class="wrapper">
+  <Animated frames={FRAMES} let:using={{ map }}>
     <PixelMap scale={5} {map} />
   </Animated>
 </div>
